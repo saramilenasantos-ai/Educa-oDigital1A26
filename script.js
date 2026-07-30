351b611a9d3037597f66f7b779d66d59ace9c953
saramilenasantos-ai <sara.milena.santos@escola.pr.gov.br>
2026-07-30 10:12:17 -0300

    alterações

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const botoes = document.querySelectorAll("button");
 
         botoes.forEach(function (botao) {
             let curtiu = false;
-
             botao.addEventListener("click", botaoClicado);
-
             function botaoClicado() {
+                console.log("fui clicado")
                 let texto = botao.querySelector("span");
 
                 if (curtiu === false) {
